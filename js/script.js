401265adde144d24e3d46f609648ee4a84a98624
Davi-GCL <1davi.gabriel1@gmail.com>
2022-12-26 22:40:14 -0300

    ver0.4

--- js/script.js
+++ js/script.js
@@ -46,6 +46,7 @@ function addTask(task){
     inputTask.value = "";
 };
 
+//Cria um elemento li(.card-tarefa) por js
 function createTagLi(task){
     let li = document.createElement("li");
     li.classList.add("card-task");
@@ -58,7 +59,7 @@ function createTagLi(task){
     let buttonArea = document.createElement("div");
     buttonArea.classList.add("button-area");
     
-    //Template para o js criar o botao de editar tarefa
+ //Template para o js criar o botao de editar tarefa
     let buttonEdit = document.createElement("button");
     buttonEdit.classList.add("button-edit-task");
     // buttonEdit.setAttribute("id", "btnEditTask");
@@ -66,22 +67,26 @@ function createTagLi(task){
     buttonEdit.addEventListener("click", () => {edit(task)});
     buttonEdit.innerHTML = `<i class="fa-solid fa-pen"></i>`;
 
-    //Template para o js criar o botao de remover tarefa
+ //Template para o js criar o botao de remover tarefa
     let buttonRemove = document.createElement("button");
     buttonRemove.classList.add("button-remove-task");
     // buttonRemove.setAttribute("id", "btnRemoveTask");
     buttonRemove.setAttribute("onclick", 'remove('+task.id+')');
     buttonRemove.innerHTML = `<i class="fa-solid fa-trash"></i>`;
 
+ //Inclui os botoes gerados dentro da div(.button-area)
     buttonArea.appendChild(buttonEdit);
     buttonArea.appendChild(buttonRemove);
 
+ //Inclui os elementos gerados h4, div, pra dentro da li(.card-task)
     li.appendChild(title);
     li.appendChild(buttonArea);
 
+ //Retorna o componente gerado, posteriormente este elemento sera incluido ao elemento pai
     return li;
 }
 
+// Funcao para editar o titulo/nome da tarefa
 function edit(task){
     if(window.confirm('Tem certeza que deseja alterar tarefa:  \"' + task.name + '\" ?') == true){
         let li = document.getElementById(''+ task.id + '');
@@ -93,9 +98,17 @@ function edit(task){
      
     }
    
-}
+};
 
-function remove(idTask){
+// Algoritmo que gera uma funcao de delay 
+
+const timer = (seconds) =>  {
+    let time = seconds * 1000
+    return new Promise(res => setTimeout(res, time))
+};
+
+// Funcao para excluir uma tarefa (remove um card de tarefa apos o fim da animacao de esmaecer(delay))
+async function remove(idTask){
     console.log(idTask);
 
     let confirm = window.confirm('Tem certeza que deseja excluir?');
@@ -104,14 +117,35 @@ function remove(idTask){
         let li = document.getElementById(''+ idTask + '');
         
         if(li){ // Se o li for retornado
+
+            li.classList.add('exclude');
+            await timer(1.4); //Aguarda o intervalo para a animacao terminar
+            
             taskList.removeChild(li);
         }
     }
-}
+};
+
+
+// function remove(idTask){
+//     console.log(idTask);
+
+//     let confirm = window.confirm('Tem certeza que deseja excluir?');
+
+//     if(confirm == true){
+//         let li = document.getElementById(''+ idTask + '');
+        
+//         if(li){ // Se o li for retornado
+//             li.classList.add('exclude');
+            
+//             // taskList.removeChild(li);
+//         }
+//     }
+// }
 
 
 
-// Algoritmo que cria uma tarefa de exemplo quando o usuario entra no site
+// Algoritmo que cria uma tarefa de exemplo para quando o usuario entra no site pela primeira vez
 
 function setupEx(){
     
@@ -126,5 +160,5 @@ function setupEx(){
 if(dataBase == 0){
     
     setupEx();
-}
+};
     
